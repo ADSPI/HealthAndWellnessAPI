e2cbef3155d7e69b769704101540c152052723e3
Renan Pereira <renan.stos077@gmail.com>
2020-05-22 22:09:16 -0300

    urls

---
+++ hwapi/healthwellness/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HealthwellnessConfig(AppConfig):
+    name = 'healthwellness'
--- hwapi/settings.py
+++ hwapi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hwapi.auth',
+    'hwapi.healthwellness',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
