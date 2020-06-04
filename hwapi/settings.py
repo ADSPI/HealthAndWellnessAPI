e7eb6717cbecddd155ffeb63d7e4c5a16c24f212
Renan Pereira <renan.stos077@gmail.com>
2020-06-04 02:49:07 -0300

    debug false

--- hwapi/settings.py
+++ hwapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_2gz*!3dv3ib09c6d2+)loqipq1xo94og*t4vn1-*%*y_1((lz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
